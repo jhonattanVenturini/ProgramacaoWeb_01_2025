4ec890d2d054d83261ee27be5e81350f11d393e3
jhonattanVenturini <jhonattan.venturini@outlook.com>
2025-03-24 14:56:04 -0300

    variaveis&constantes

---
+++ JS/Aula01_JS/main.js
@@ -0,0 +1,10 @@
+
+// Aula 01 - Variaveis e junção de texto
+
+let nomeAluno = "Jhonattan Venturini"
+let idadeAluno = 22
+let praia = true
+
+console.log('Olá meu nome é:', nomeAluno , 'minha idade é:' , idadeAluno , 'eu gosto de ',praia)
+
+
