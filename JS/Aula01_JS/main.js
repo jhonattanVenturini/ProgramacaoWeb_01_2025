a40f05954927151ef88fe4b1f37c85f88dd2c883
jhonattanVenturini <jhonattan.venturini@outlook.com>
2025-03-24 17:34:24 -0300

    atividade

--- JS/Aula01_JS/main.js
+++ JS/Aula01_JS/main.js
@@ -1,10 +1,19 @@
 
 // Aula 01 - Variaveis e junção de texto
 
+console.log('Olá, mundo')
+
 let nomeAluno = "Jhonattan Venturini"
 let idadeAluno = 22
 let praia = true
 
 console.log('Olá meu nome é:', nomeAluno , 'minha idade é:' , idadeAluno , 'eu gosto de ',praia)
 
+// teste de programação IMC
+
+let altura = 1.77
+let peso = 75
+let imc = peso / (altura * altura);
+
+console.log(imc);
 
