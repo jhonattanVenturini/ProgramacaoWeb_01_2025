f8a7fcfad9fda6ceaf35b10c3fb971c58d219c72
jhonattanVenturini <jhonattan.venturini@outlook.com>
2025-04-08 17:13:09 -0300

    aula09-dowhile&while

---
+++ JS/Aula09/dowhile.js
@@ -0,0 +1,10 @@
+
+// estruta de repetição while enquanto esse cara tiver executando
+// vc faz isso:
+
+let temCafe = false
+
+do {
+console.log("O shadow bebe café");
+
+} while (temCafe == true);
